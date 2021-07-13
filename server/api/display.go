17491582178829ebfaced011da0d2033a537f7d2
kt <eru08tera15mora@gmail.com>
2021-07-14 01:52:33 +0900

    add Display API

---
+++ server/api/display.go
@@ -0,0 +1,21 @@
+package api
+
+import (
+	"github.com/andybrewer/mack"
+)
+
+func UnSleep() string {
+	osa, err := mack.Tell("System Events", "do shell script \"caffeinate -u -t 1\"")
+	if err != nil {
+		return err.Error()
+	}
+	return osa
+}
+
+func Sleep() string {
+	osa, err := mack.Tell("Finder", "sleep")
+	if err != nil {
+		return err.Error()
+	}
+	return osa
+}
