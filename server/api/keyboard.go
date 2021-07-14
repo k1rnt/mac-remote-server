3343792f517ff0838bc8d30c6bfc2c307931d255
kt <eru08tera15mora@gmail.com>
2021-07-15 01:31:12 +0900

    add keyboard api

---
+++ server/api/keyboard.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	"fmt"
+
+	"github.com/andybrewer/mack"
+)
+
+func Keycode(code int) string {
+	osa, err := mack.Tell("System Events", fmt.Sprintf("key code %d", code))
+	if err != nil {
+		return err.Error()
+	}
+	return osa
+}
+
+func Keystroke(isSpecial bool, key string) string {
+	var cmd string
+	if isSpecial {
+		cmd = fmt.Sprintf("keystroke %s", key)
+	} else {
+		cmd = fmt.Sprintf("keystroke \"%s\"", key)
+	}
+
+	osa, err := mack.Tell("System Events", cmd)
+	if err != nil {
+		return err.Error()
+	}
+	return osa
+}
