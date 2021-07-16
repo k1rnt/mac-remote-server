3ce160ad77ba83ea9065fcd581f83121064daa99
kt <eru08tera15mora@gmail.com>
2021-07-17 01:31:18 +0900

    add keycode handler

--- server/handler/handle.go
+++ server/handler/handle.go
@@ -5,6 +5,7 @@ import (
 	"github.com/valyala/fasthttp"
 
 	"github.com/k1rnt/mac-remote-server/api"
+	"github.com/k1rnt/mac-remote-server/models"
 )
 
 func UnSleep() echo.HandlerFunc {
@@ -16,5 +17,15 @@ func UnSleep() echo.HandlerFunc {
 func Sleep() echo.HandlerFunc {
 	return func(c echo.Context) (err error) {
 		return c.String(fasthttp.StatusCreated, api.Sleep())
+	}
+}
+
+func Keycode() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		var k models.Keycode
+		if err := c.Bind(&k); err != nil {
+			return c.String(fasthttp.StatusBadRequest, err.Error())
+		}
+		return c.String(fasthttp.StatusCreated, api.Keycode(k.Code))
 	}
 }
